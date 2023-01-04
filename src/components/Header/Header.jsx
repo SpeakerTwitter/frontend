b44c293d4a58b1b3652f8f182020406235ddc7c6
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-04 15:49:48 -0600

    some styling and classnames added. Home button is at the top

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,12 @@ import { Link } from 'react-router-dom'
  
 const Header = (props) => {
    return (
-       <header style={{ height: "300px", overflow: 'hidden' }}>
+       <header style={{overflow: 'hidden' }}>
            <nav className="nav">
                <Link to='/'>
-                   <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png" />
+                    <h1>Home</h1>
                </Link>
            </nav>
-           <img src="https://tse4.mm.bing.net/th?id=OIP.5yGbSisW4B8-BflHQXeO4QHaEo&pid=Api&P=0" />
        </header>
    )
 }
