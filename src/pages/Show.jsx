f8b63bb6dd0434e8186f2cce9e46f024385796b7
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-04 13:14:38 -0600

    the show page almost done. still need to create the function which will delete a tweet

---
+++ src/pages/Show.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react'
+import { useParams, Link, useNavigate } from 'react-router-dom'
+ 
+const placeholderImage = "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+ 
+const Show = (props) => {
+   // local state (Show)
+   const [tweet, setTweet] = useState(null)
+   const [loading, setLoading] = useState(true)
+   // access information about the current url path for browser
+ 
+   const { id } = useParams()
+   const navigate = useNavigate()
+   // define some local variables
+   const URL = `http://localhost:4000/tweet/${id}`
+ 
+   const getTweet = async () => {
+       try {
+           const response = await fetch(URL)
+           const result = await response.json()
+           console.log(result)
+           setTweet(result)
+           setLoading(false)
+       } catch (err) {
+           console.log(err)
+       }
+   }
+   // make a fetch
+ 
+   
+   const isLoading = () => (<h2>....Loading</h2>)
+   const loaded = () => (
+       <>
+           <div className="tweet-card">
+               {/* React optimization / difference */}
+               <h1>{tweet.name}</h1>
+               <div>
+                   <p>Delete Tweet</p>
+                   <button onClick={removeTweet}> X </button>
+               </div>
+               <img src={tweet.image || placeholderImage} />
+               <h3>{tweet.title || "Not tweet given"}</h3>
+           </div>
+           <Link to="/">Back to Home</Link>
+       </>
+   )
+   useEffect(() => { getTweet() }, [id,loading])
+   // confirm + render JSX +++
+   // console.log(`current person: ${person?._id || "no person"}`)
+   return (
+       <section className="ShowContainer">
+ 
+           {loading ? isLoading() : loaded()}
+ 
+       </section>)
+}
+ 
+export default Show
