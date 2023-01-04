df56f0bee3f0aa236e160a97c1adee0eaf8d8394
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-04 12:55:36 -0600

    worked on main.jsx and spoke with backend to connect the routes to the correct page

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import {Routes, Route } from 'react-router-dom'
+import Tweet from '../pages/Tweet/Tweet'
+import Show from '../pages/Show'
+ 
+const Main = (props) => {
+ return (
+   <main>
+       <Routes>
+           <Route path='/' element={<Tweet/>}/>
+           <Route path='/tweet/:id' element={<Show />}/>
+       </Routes>
+ 
+   </main>
+ )
+}
+ 
+export default Main
