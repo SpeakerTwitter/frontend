0c3bf3ea677d89f96c01ad01fa562bab2b4fa9dc
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-04 12:50:59 -0600

    created App.js content for what will be shown on first page

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from '../Header/Header'
+import Main from '../Main'
+
+function App() {
+ return (
+   <div className='App'>
+       <Header />
+       <Main />
+   </div>
+ );
+}
+export default App
