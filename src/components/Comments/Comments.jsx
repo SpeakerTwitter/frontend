846f5e6f0a09a7e6ead8526e93d10050b26a278c
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-06 09:28:28 -0600

    input loading function

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -125,7 +125,21 @@ const Comments = () => {
       </div>
     );
   };
-
+  
+  // Loading
+  const loading = () => (
+    <section className="loading">
+      <h1>
+        Loading...
+        <span>
+          <img
+            className="spinner"
+            src="https://freesvg.org/img/1544764567.png"
+          />{" "}
+        </span>
+      </h1>
+    </section>
+  );
 
 
 
