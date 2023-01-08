bc8a4171e18a16977bb10cff068b5a2febf804c1
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-08 17:25:39 -0600

    can take you back to the top of the page once at the bottom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,19 @@
-import './App.css';
+import "./App.css";
 
-import Header from '../Header/Header'
-import Main from '../Main'
+import Header from "../Header/Header";
+import Main from "../Main";
 
 function App() {
- return (
-   <div className='App'>
-       <Header />
-       <Main />
-       <h1 className="footer">Return to Top of Page</h1>
-   </div>
- );
+  return (
+    <div className="App">
+      <Header />
+      <a name="top"></a>
+
+      <Main />
+      <a className="footer" href="#top">
+        Back to top
+      </a>
+    </div>
+  );
 }
-export default App
+export default App;
